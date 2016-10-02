779d598e6a64344ff482c32694611b4d3e611dbd
Kelly Mears <code@kellymears.me>
2016-10-02 03:53:47 -0700

    split index into dynamic parts

---
+++ parts/footer.php
@@ -0,0 +1,17 @@
+<footer>
+  <div class="row">
+    <div class="large-5 columns">
+    <p>This informational campaign is brought to you by The Other 98%.</p>
+    </div>
+  </div>
+</footer>
+
+
+<script src="js/vendor/jquery.js"></script>
+<script src="js/foundation.min.js"></script>
+<script>
+  $(document).foundation();
+</script>
+
+</body>
+</html>
---
+++ parts/header.php
@@ -0,0 +1,169 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+
+    <!--
+
+      █▓▒▓█▀██▀█▄░░▄█▀██▀█▓▒▓█
+      █▓▒░▀▄▄▄▄▄█░░█▄▄▄▄▄▀░▒▓█
+      █▓▓▒░░░░░▒▓░░▓▒░░░░░▒▓▓█
+         we've seen enough.
+
+      ▄██████████████▄▐█▄▄▄▄█▌
+      ██████▌▄▌▄▐▐▌███▌▀▀██▀▀
+      ████▄█▌▄▌▄▐▐▌▀███▄▄█▌
+      ▄▄▄▄▄██████████████▀
+           call it off.
+
+      == website by quaid ==
+
+    -->
+
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <title>Dakota Access Pipeline</title>
+    <meta name="copyright" content="This Bad Idea brought to you by Energy Transfer">
+	  <meta http-equiv="X-UA-Compatible" content="IE=EmulateIE9" />
+    <meta name="description" content="The Dakota Access pipeline is proposed to transport crude oil from the North Dakota Bakken region, endangering the lives and livelihoods of everyone in its path.">
+
+    <!-- css -->
+    <link rel="stylesheet" href="css/foundation.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
+
+    <!-- js -->
+    <script src="js/vendor/modernizr.js"></script>
+
+    <!--
+     SERVE AMP VER for home
+     https://developers.google.com/search/docs/guides/use-AMP-HTML -->
+
+    <link rel="canonical" href="http://dakotaaccesspipeline.net/index.amp.html" />
+
+    <!-- JSON-LD meta data
+    http://json-ld.org/playground/ -->
+    <script type="application/ld+json">
+
+     {
+      "http://schema.org/description": [
+        {
+          "@value": "The Dakota Access Pipelineis a massive oil pipeline that poses great danger to the people of North Dakota and the state's renown farmland."
+        }
+      ],
+      "http://schema.org/geo": [
+        {
+          "http://schema.org/latitude": [
+            {
+              "@type": "http://www.w3.org/2001/XMLSchema#float",
+              "@value": "46.38"
+            }
+          ],
+          "http://schema.org/longitude": [
+            {
+              "@type": "http://www.w3.org/2001/XMLSchema#float",
+              "@value": "100.59"
+            }
+          ]
+        }
+      ],
+      "http://schema.org/image": [
+        {
+          "@id": "img/dakota-access-pipeline.jpg"
+        }
+      ],
+      "http://schema.org/name": [
+        {
+          "@value": "Dakota Access Pipeline"
+        }
+      ]
+     }
+
+     </script>
+
+  </head>
+
+  <body>
+
+    <header>
+
+	      <div class="row">
+
+		      <div class="medium-8 columns">
+            <img id="logo" src="img/logo.svg" />
+			      <h1>Dakota Access Pipeline (DAPL)</h1>
+		      </div>
+
+		      <div class="medium-4 hide-for-small columns">
+
+			      <ul id="header-nav">
+				      <li><a href="#" class="button alert large-expanded small-expanded expand">Subscribe To DAPL Spill Alerts</a></li>
+              <li><a href="#" class="button success large-expanded small-expanded expand"><strong>Having a Disaster Mitigation Plan</a></li>
+			      </ul>
+
+		      </div>
+
+	      </div>
+
+    </header>
+
+    <div class="page-wrapper">
+      <div class="row page-container">
+
+        <div class="row">
+          <div class="large-12 columns">
+            <nav class="top-bar" data-topbar role="navigation">
+            <ul class="title-area">
+              <li class="name">
+              </li>
+               <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
+              <li class="toggle-topbar menu-icon"><a href="index.html#"><span>Menu</span></a></li>
+            </ul>
+
+            <section class="top-bar-section">
+              <!-- Left Nav Section -->
+              <ul class="left">
+                <li class="has-dropdown">
+                  <a href="index.html#">About the Dakota Access Pipeline</a>
+                  <ul class="dropdown">
+                <li><a href="about/overview.html">Overview</a></li>
+                <li><a href="about/route.html">The Route</a></li>
+                <li><a href="about/community.html">In the Community</a></li>
+              </ul>
+                </li>
+                <li class="has-dropdown">
+                  <a href="index.html#">Dakota Access Pipeline Dangers</a>
+                  <ul class="dropdown">
+                <li><a href="resources/faq.html">Frequently Asked Questions</a></li>
+                <li><a href="resources/project-filings.html">Project Filings</a></li>
+                <li><a href="resources/project-maps.html">Project Maps</a></li>
+                <li><a href="resources/presentations.html">Presentations</a></li>
+              </ul>
+                </li>
+                <li class="has-dropdown">
+                  <a href="index.html#">Landowner Issues</a>
+                  <ul class="dropdown">
+                  <li><a href="landowner-facts/landowner-qa.html">Landowner Q&A</a></li>
+                <li><a href="landowner-facts/environmental-protection.html">Environmental Protection</a></li>
+                <li><a href="landowner-facts/agricultural-plan.html">Agricultural Plan</a></li>
+                <li><a href="landowner-facts/landowner-property.html">Landowner Property</a></li>
+                  </ul>
+                </li>
+                <li class="has-dropdown">
+                  <a href="index.html#">How To Keep Your Family Safe</a>
+                  <ul class="dropdown">
+                <li><a href="project-safety/national-pipeline-safety.html">National Pipeline Safety</a></li>
+                <li><a href="project-safety/safety-commitment.html">Dakota Access’ Commitment to Safety</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </section>
+          </nav>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="large-12 columns">
+            <img src="img/dakota-map.jpg" alt="map"/>
+          </div>
+        </div>
