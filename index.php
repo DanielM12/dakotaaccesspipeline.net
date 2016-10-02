138e7190c2eb53778c139c20ac698d037b0cb038
Kelly Mears <code@kellymears.me>
2016-10-02 04:26:27 -0700

    removed sitesucker garbage; removed index.html

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 
         <h3>The Dakota Access Pipeline Is Flat-Out Dangerous.</h3>
 
-        <p>The <strong>Dakota Access Pipeline</strong> is a new 1,172-mile pipeline that will connect the rapidly expanding Bakken and Three Forks production areas in North Dakota to Patoka, Illinois. The <strong>Dakota Access Pipeline</strong> will make it more profitable for Energy Transfer to ship crude oil from North Dakota to existing refineries. While cost-effective, the <strong>Dakota Access Pipeline</strong> is downright dangerous for North Dakotans &mdash; especially farmers and others who work off the land.</p>
+        <p>The <strong>Dakota Access Pipeline</strong> is a new 1,172-mile pipeline that will connect the Bakken and Three Forks production areas in North Dakota to Patoka, Illinois. The <strong>Dakota Access Pipeline</strong> will make it more profitable for Energy Transfer to ship crude oil from North Dakota to existing refineries. While cost-effective, the <strong>Dakota Access Pipeline</strong> is downright dangerous for North Dakotans &mdash; especially farmers and others who work off the land.</p>
 
         <h3>The Dakota Access Pipeline Spill Will Ruin North Dakota's Water Forever</h3>
         <p>The Dakota Access Pipeline will transport approximately 470,000 barrels per day with a capacity as high as 570,000 barrels per day or more – which could represent approximately half of Bakken current daily crude oil production. <strong>In the event of a spill, all of this oil would be likely to seep into the groundwater of North Dakota, ruining agricultural endeavors and poisoning residents.</strong></p>
