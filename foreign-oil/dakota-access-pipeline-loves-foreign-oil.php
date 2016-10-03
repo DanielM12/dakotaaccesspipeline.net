e05e9ecfae803a2c98129c342d9e3b7fde00eae3
Kelly Mears <code@kellymears.me>
2016-10-03 01:34:52 -0700

    lil tweaks

--- foreign-oil/dakota-access-pipeline-loves-foreign-oil.php
+++ foreign-oil/dakota-access-pipeline-loves-foreign-oil.php
@@ -6,10 +6,10 @@
 						<hr/>
             <p>We know Energy Transfer, LLC has said a lot about how this pipeline will reduce our dependence on foreign oil. For instance, the time Dakota Access said of the Standing Rock protests:</p>
             <p><blockquote>“It’s a shameful act by a group of people trying to disrupt our energy security and independence,” <cite>Dakota Access Officials, http://apne.ws/2cLNofc</cite></blockquote></p>
-            <p>The thing is, despite the detached corporate theatrics, Energy Transfer, LLC plans on exporting the oil that comes through the pipeline. As Energy Transfer, LLC presented
+            <p>The thing is, despite the insincere flag waving, Energy Transfer, LLC plans on exporting the oil that comes through the pipeline. As Energy Transfer, LLC presented
               to bigwig corporate Citi Group investors:</p>
             <img src="http://dakotaaccesspipeline.net/img/busted.png" style="width:100%;" alt="Dakota Access Pipeline DAPL is excited to explore foreign oil export options!">
-            <a class="button" href="http://bit.ly/2cLMb7J">You can see the full PDF on how little they care about America here.</a>
+            <a class="button" href="http://bit.ly/2cLMb7J">View the full leaked PDF on how little DAPL cares about America here.</a>
 			    </div>
 
 			    <?php require("../parts/side-nav.php"); ?>
