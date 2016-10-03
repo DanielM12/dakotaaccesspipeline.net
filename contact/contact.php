b16cdc8e90db43cb6ec964b14f426802d9cba2d6
Kelly Mears <code@kellymears.me>
2016-10-02 20:09:14 -0700

    Contact section

---
+++ contact/contact.php
@@ -0,0 +1,23 @@
+<?php require("../parts/header.php"); ?>
+
+		    <div class="row">
+			    <div class="large-8 columns">
+				    <h4>Contact the Dakota Access Pipeline (DAPL)</h4>
+				    <hr/>
+
+						<p>The Dakota Access Pipeline has set up a toll-free contact number for
+							general concerns about the Pipeline. They are ready to hear your thoughts!</p>
+
+						<h2>1-855-430-4491</h2>
+
+						<hr/>
+
+			    </div>
+
+			    <?php require("../parts/side-nav.php"); ?>
+		    </div>
+
+	    </div>
+    </div>
+
+<?php require("../parts/footer.php"); ?>
