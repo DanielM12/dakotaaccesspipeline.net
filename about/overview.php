08f71d5a5c8960e8f2add7689c6e91eb346019b1
Kelly Mears <code@kellymears.me>
2016-10-03 02:34:44 -0700

    lil tweaks -- burning out

--- about/overview.php
+++ about/overview.php
@@ -20,11 +20,20 @@
             <li>Dakota Access LLC is a garbage company that deserves to be punished by the federal government and investors.</li>
 					</ul>
 
+					<blockquote>
+						<p>The Standing Rock Sioux Tribe will continue to explore all legal, legislative, and administrative options to stop construction of the Dakota Access Pipeline. The pipeline has already led to the
+							destruction of our sacred sites.</p>
+						<p>It is unfortunate that the corporate world chooses to ignore the millions of people and hundreds of tribal nations who stand in opposition to the destruction of our lands, resources, waters, and sacred
+							sites.</p>
+						<p>Energy Transfer Partners has demonstrated time and time again that the bottom line for them is money. The bottom line for the Standing Rock Sioux Tribe is and will always be protecting our lands, people,
+							water, and sacred sites from the devastation of this pipeline. Our fight isn’t over until there is permanent protection of our people and resources from the pipeline.</p>
+							<cite>Standing Rock Sioux Statement, http://bit.ly/2dDjET6</cite></blockquote>
+
 
 			    </div>
 
 			    <?php require("../parts/side-nav.php"); ?>
-					
+
 		    </div>
 
 	    </div>
