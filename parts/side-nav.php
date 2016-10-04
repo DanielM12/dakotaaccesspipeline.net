b3b4b94bd65501a69b41b4acd1ddaa6adb10bf2f
Kelly Mears <code@kellymears.me>
2016-10-03 22:13:14 -0700

    pretty URLs

--- parts/side-nav.php
+++ parts/side-nav.php
@@ -2,11 +2,11 @@
   <div id="side-nav">
     <ul>
     <li><h5><em>Quick Links</em></h5></li>
-      <li><a href="http://dakotaaccesspipeline.net/about/fact-sheet.php">Fact Sheet</a></li>
-      <li><a href="http://dakotaaccesspipeline.net/about/route.php">The Route</a></li>
-      <li><a href="http://dakotaaccesspipeline.net/landowner-facts/landowner-qa.php">Landowner Issues</a></li>
-      <li><a href="http://dakotaaccesspipeline.net/about/community.php">DAPL "In The Community"</a></li>
-      <li><a href="http://dakotaaccesspipeline.net/contact/contact.php">Contact Us</a></li>
+      <li><a href="http://dakotaaccesspipeline.net/about/fact-sheet">Fact Sheet</a></li>
+      <li><a href="http://dakotaaccesspipeline.net/about/route">The Route</a></li>
+      <li><a href="http://dakotaaccesspipeline.net/landowner-facts/landowner-qa">Landowner Issues</a></li>
+      <li><a href="http://dakotaaccesspipeline.net/about/community">DAPL "In The Community"</a></li>
+      <li><a href="http://dakotaaccesspipeline.net/contact/contact">Contact Us</a></li>
       </ul>
   </div>
 </div>
